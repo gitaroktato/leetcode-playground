55016d8f045ca5a4a820531fe4fd274123a9d754
gitaroktato <oresztesz_margaritisz@epam.com>
2025-09-05 14:04:34 +0200

    feat: LinkedList solution

--- lru-cache/main.py
+++ lru-cache/main.py
@@ -1,3 +1,6 @@
+from typing import NamedTuple
+
+
 def find_min_key(timer_dict):
     min_key = None
     min_value = None
@@ -8,26 +11,37 @@ def find_min_key(timer_dict):
     return min_key
 
 
+class CacheContent(NamedTuple):
+    key: int
+    value: int
+
+
 class LRUCache(object):
     def __init__(self, capacity):
         """
         :type capacity: int
         """
         self._capacity = capacity
-        self._cache = dict()
-        self._ages = dict()
-        self._timer = 0
+        self._cache: dict[int, int] = dict()
+        self._content: list[CacheContent] = list()
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        self._timer += 1
-        value = self._cache.get(key, -1)
-        if value != -1:
-            self._ages[key] = self._timer
-        return value
+        index = self._cache.get(key, -1)
+        if index != -1:
+            # Move element to tail
+            element = self._content[index]
+            del self._content[index]
+            self._content.append(element)
+            self._cache[key] = len(self._content) - 1
+            for others in self._content[index:-1]:
+                self._cache[others.key] -= 1
+            # Return the value
+            return element.value
+        return -1
 
     def put(self, key, value):
         """
@@ -35,13 +49,20 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
-        self._timer += 1
-        self._cache[key] = value
-        self._ages[key] = self._timer
+        # Move new element to tail
+        index = self._cache.get(key, -1)
+        if index != -1:
+            del self._content[index]
+            for others in self._content[index:]:
+                self._cache[others.key] -= 1
+        self._content.append(CacheContent(key, value))
+        self._cache[key] = len(self._content) - 1
         if len(self._cache) > self._capacity:
             self.evict()
 
     def evict(self):
-        min_key = find_min_key(self._ages)
-        del self._cache[min_key]
-        del self._ages[min_key]
+        first = self._content[0]
+        del self._content[0]
+        del self._cache[first.key]
+        for element in self._content:
+            self._cache[element.key] -= 1
--- lru-cache/main_test.py
+++ lru-cache/main_test.py
@@ -23,3 +23,50 @@ def test_eviction():
     cache.put(3, 3)
     # Least recent is 2
     assert cache.get(2) == -1
+
+
+def test_case_1():
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert cache.get(1) == 1
+    cache.put(3, 3)
+    assert cache.get(2) == -1
+    cache.put(4, 4)
+    assert cache.get(1) == -1
+    assert cache.get(3) == 3
+    assert cache.get(4) == 4
+
+
+def test_case_2():
+    cache = LRUCache(2)
+    assert cache.get(2) == -1
+    cache.put(2, 6)
+    assert cache.get(1) == -1
+    cache.put(1, 5)
+    cache.put(1, 2)
+    assert cache.get(1) == 2
+    assert cache.get(2) == 6
+
+
+def test_case_3():
+    cache = LRUCache(10)
+    cache.put(7, 28)
+    cache.put(7, 1)
+    cache.put(8, 15)
+    assert cache.get(6) == -1
+    cache.put(10, 27)
+    cache.put(8, 10)
+    assert cache.get(8) == 10
+    cache.put(6, 29)
+    cache.put(1, 9)
+    assert cache.get(6) == 29
+    cache.put(10, 7)
+    assert cache.get(1) == 9
+    assert cache.get(2) == -1
+    assert cache.get(13) == -1
+    cache.put(8, 39)
+    cache.put(1, 5)
+    assert cache.get(1) == 5
+    cache.put(13, 2)
+    assert cache.get(12) == -1
